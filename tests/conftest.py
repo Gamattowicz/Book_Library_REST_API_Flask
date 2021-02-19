8cf4eda5224e6caf21b61ab34e21735615d91922
Gamattowicz <p.romanczuk31@gmail.com>
2021-02-19 21:39:25 +0100

    add test get_authors function

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,3 +40,9 @@ def token(client, user):
         'password': user['password']
     })
     return response.get_json()['token']
+
+
+@pytest.fixture
+def sample_data(app):
+    runner = app.test_cli_runner()
+    runner.invoke(add_data)
